7f8a534d02f14fbe39364f7f6c7d43405960fe56
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-08-29 08:51:33 +0300

    update th bool isPrime variable

--- bin/prime_number_or_not.dart
+++ bin/prime_number_or_not.dart
@@ -1,20 +1,38 @@
 //quize solve problem the number is prime or not
 void main() {
+  // int number = 5;
+  // int counter = 0;
+  // for (int i = 1; i <= number; i++) {
+  //   if (number % i == 0) {
+  //     counter++;
+  //   }
+  // }
+  // if (counter == 2) {
+  //   print("the number $number is Prime");
+  // } else {
+  //   print("the number $number is N'tPrime");
+  // }
+  // if (counter == 2) {
+  //     print("the number $number is Prime");
+  //   } else {
+  //     print("the number $number is N'tPrime");
+  //   }
+  // another way *******************
   int number = 5;
-  int counter = 0;
-  for (int i = 1; i <= number; i++) {
-    if (number % i == 0) {
-      counter++;
+  bool isPrime=false;
+  for(int i =2; i<=number;i++){
+    if(number%i==0){
+      isPrime=true;
+      break;
     }
   }
-  if (counter == 2) {
+  if(isPrime){
     print("the number $number is Prime");
-  } else {
-    print("the number $number is N'tPrime");
-  }
-  if (counter == 2) {
-      print("the number $number is Prime");
-    } else {
-      print("the number $number is N'tPrime");
-    }
+      } else {
+        print("the number $number is N'tPrime");
+      }
+
+
+
+
 }
