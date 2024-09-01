bfd58f5439f0076ed4020f6e508caac18368a34c
Problem solving-Abdo <ABOabdomostafam@gmail.com>
2024-09-01 07:50:06 +0300

    Update function prime_number_or_not.dart

--- bin/prime_number_or_not.dart
+++ bin/prime_number_or_not.dart
@@ -1,3 +1,5 @@
+
+import 'dart:io';
 //quize solve problem the number is prime or not
 void main() {
   // int number = 5;
@@ -18,7 +20,7 @@ void main() {
   //     print("the number $number is N'tPrime");
   //   }
   // another way *******************
- print("enter the number");
+/* print("enter the number");
   int number = int.parse(stdin.readLineSync());
   bool isPrime = true;
   for (int i = 2; i <= number; i++) {
@@ -32,8 +34,32 @@ void main() {
   } else {
     print("No");
   }
+*/
+//****************another way***************
+print(isPrime(7));
+}
+// bool isPrime(int number){
+//   bool isPrime=true;
+//   for(int i=2; i<number;i++){
+//     if(number%i==0){
+//       isPrime=false;
+//       break;
+//     }
+//   }
+//   return isPrime;
+// }
+ isPrime(int number){
+ int counter =  0;
+ bool isPrime= true;
+ for(int i=1; i<=number;i++){
+    if(number%i==0){
 
-
-
-
+      counter++;
+    }
+  }
+ if(counter==2) {
+   return isPrime;
+ }else{
+   return isPrime =false;
+ }
 }
